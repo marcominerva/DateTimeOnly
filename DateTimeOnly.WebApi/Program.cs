06fe5dac2579c380c1f32a4708f1e7723fbb8f3a
Marco Minerva <marco.minerva@gmail.com>
2022-05-24 10:55:07 +0200

    Add DateOnly and TimeOnly JSON converters

---
+++ DateTimeOnly.WebApi/Program.cs
@@ -0,0 +1,32 @@
+using DateTimeOnly.Converters;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new DateOnlyConverter());
+    options.JsonSerializerOptions.Converters.Add(new TimeOnlyConverter());
+});
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    _ = app.UseSwagger();
+    _ = app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
--- DateTimeOnly/Program.cs
+++ DateTimeOnly/Program.cs
@@ -1,13 +1,34 @@
-﻿var dateOnly = DateOnly.FromDateTime(DateTime.Now);
+﻿using System.Text.Json;
+using DateTimeOnly.Converters;
+
+var dateOnly = DateOnly.FromDateTime(DateTime.Now);
 var timeOnly = TimeOnly.FromDateTime(DateTime.Now);
 
 Console.WriteLine(dateOnly);
 Console.WriteLine(timeOnly);
 
+var jsonSerializerOptions = new JsonSerializerOptions(JsonSerializerDefaults.Web);
+jsonSerializerOptions.Converters.Add(new DateOnlyConverter());
+jsonSerializerOptions.Converters.Add(new TimeOnlyConverter());
+
+var invoice = new Invoice { Amount = 42, Date = dateOnly };
+var json = JsonSerializer.Serialize(invoice, jsonSerializerOptions);
+
+Console.WriteLine(json);
+
+invoice = JsonSerializer.Deserialize<Invoice>(json, jsonSerializerOptions);
+Console.WriteLine(invoice.Date);
+
+var restaurant = new Restaurant { Name = "La Darsena", OpeningTime = new TimeOnly(19, 00), ClosingTime = new TimeOnly(23, 30) };
+json = JsonSerializer.Serialize(restaurant, jsonSerializerOptions);
+
+Console.WriteLine(json);
+
 Console.ReadLine();
 
 public class Invoice
 {
+    //[JsonConverter(typeof(DateOnlyConverter))]
     public DateOnly Date { get; set; }
 
     public double Amount { get; set; }
@@ -17,7 +38,9 @@ public class Restaurant
 {
     public string Name { get; set; }
 
+    //[JsonConverter(typeof(TimeOnlyConverter))]
     public TimeOnly OpeningTime { get; set; }
 
+    //[JsonConverter(typeof(TimeOnlyConverter))]
     public TimeOnly ClosingTime { get; set; }
 }
