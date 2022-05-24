6284fc87c6eb659fcf6c7d1fa3e3d6e003f2a8ba
Marco Minerva <marco.minerva@gmail.com>
2022-05-24 10:16:40 +0200

    Initial commit

---
+++ DateTimeOnly/Program.cs
@@ -0,0 +1,23 @@
+﻿var dateOnly = DateOnly.FromDateTime(DateTime.Now);
+var timeOnly = TimeOnly.FromDateTime(DateTime.Now);
+
+Console.WriteLine(dateOnly);
+Console.WriteLine(timeOnly);
+
+Console.ReadLine();
+
+public class Invoice
+{
+    public DateOnly Date { get; set; }
+
+    public double Amount { get; set; }
+}
+
+public class Restaurant
+{
+    public string Name { get; set; }
+
+    public TimeOnly OpeningTime { get; set; }
+
+    public TimeOnly ClosingTime { get; set; }
+}
